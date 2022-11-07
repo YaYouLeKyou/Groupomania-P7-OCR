3fbb1ef169e0f25dad3a5e8c77dcb17dbdf1f770
yayou <yanes75@hotmail.fr>
2022-11-07 20:46:01 +0100

    refacto(Header.vue):added tailwindcss

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,46 +1,49 @@
 <template>
-  <header>
-    <nav>
-      <div class="logo">
-        <a href="http://localhost:8081/welcome">
+  <header class="container mx-auto border-b-2 border-green-400 bg-gray-900">
+    <nav class="flex items-center justify-between flex-wrap p-6">
+      <div class="flex items-center flex-no-shrink text-white mr-6">
+        <a href="http://localhost:8080/welcome">
           <img
             width="158"
             height="78"
             src="../assets/icon-white.png"
-            alt="linkmania-logo"
+            alt="groupomania-logo"
           />
         </a>
       </div>
-      <div class="button-menu">
-        <button class="button-cliquer-menu" aria-label="Cliquer sur menu">
-          <svg class="menu" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+      <div class="block lg:hidden">
+        <button
+          class="flex items-center px-3 py-2 border rounded text-teal-lighter bg-green-400 border-teal-light hover:text-white hover:border-white"
+          aria-label="Cliquer sur menu"
+        >
+          <svg class="h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <title>Menu</title>
             <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
           </svg>
         </button>
       </div>
-      <div class="link">
-        <div class="text-link">
+      <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+        <div class="text-sm lg:flex-grow">
           <router-link
             to="/feed"
-            href="http://localhost:8081/feed"
-            class="feed"
+            href="http://localhost:8080/feed"
+            class="block mt-4 lg:inline-block lg:mt-0 text-lg text-teal-lighter text-green-400 hover:text-white mr-4"
             aria-label="Accéder aux articles"
           >
-            Lien Groupomania
+            Groupo'Link
           </router-link>
           <router-link
             to="/profile/:id"
-            href="http://localhost:8081/profile/:id"
-            class="mon-compte"
+            href="http://localhost:8080/profile/:id"
+            class="block mt-4 lg:inline-block lg:mt-0 text-lg text-teal-lighter text-white hover:text-green-400"
             aria-label="Accéder à mon compte"
           >
             Mon compte
           </router-link>
           <router-link
             to="/Users"
-            href="http://localhost:8081/Users"
-            class="membre"
+            href="http://localhost:8080/Users"
+            class="block ml-4 mt-4 lg:inline-block lg:mt-0 text-lg text-teal-lighter text-white hover:text-green-400"
             aria-label="Accéder à la page membres"
           >
             Membres
@@ -50,7 +53,7 @@
           <a
             @click="logout()"
             href="#"
-            class="se-deconnecter"
+            class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal hover:text-green-400 mt-4 lg:mt-0"
             aria-label="Se déconnecter"
           >
             Se déconnecter
@@ -64,145 +67,17 @@
 <script>
 export default {
   name: "Header",
+  props: {
+    isConnected: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    logout() {
+      localStorage.clear();
+      window.location.href = "/";
+    },
+  },
 };
 </script>
-<style>
-* {
-  font-family: "Lato", sans-serif;
-}
-header {
-  width: 100%;
-  background-color: #111827;
-  border-bottom-width: 2px;
-  border-color: #34d399;
-}
-nav {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  align-items: center;
-}
-.logo {
-  display: flex;
-  margin-right: 1.5rem;
-  color: #ffffff;
-  align-items: center;
-}
-.button-menu {
-  display: block;
-
-  @media (min-width: 1024px) {
-  }
-}
-.button-cliquer-menu {
-  display: flex;
-  padding-top: 0.5rem;
-  padding-bottom: 0.5rem;
-  padding-left: 0.75rem;
-  padding-right: 0.75rem;
-  background-color: #34d399;
-  align-items: center;
-  border-radius: 0.25rem;
-  border-width: 1px;
-}
-.button-cliquer-menu:hover {
-  color: #ffffff;
-}
-
-.menu {
-  width: 0.75rem;
-  height: 0.75rem;
-}
-.link {
-  display: block;
-  flex-grow: 1;
-  width: 100%;
-
-  @media (min-width: 1024px) {
-    display: flex;
-    align-items: center;
-    width: auto;
-  }
-}
-.text-link {
-  display: flex;
-  flex-direction: row;
-  font-size: 1rem;
-  line-height: 1.25rem;
-  color: red;
-
-  @media (min-width: 1024px) {
-    flex-grow: 1;
-  }
-}
-.feed {
-  display: block;
-  margin-top: 1rem;
-  margin-right: 1rem;
-  color: #34d399;
-  font-size: 1.125rem;
-  line-height: 1.75rem;
-
-  @media (min-width: 1024px) {
-    display: inline-block;
-    margin-top: 0;
-  }
-}
-.feed:hover {
-  color: #ffffff;
-}
-.mon-compte {
-  display: block;
-  margin-top: 1rem;
-  color: #ffffff;
-  font-size: 1.125rem;
-  line-height: 1.75rem;
-
-  @media (min-width: 1024px) {
-    display: inline-block;
-    margin-top: 0;
-  }
-}
-.mon-compte:hover {
-  color: #34d399;
-}
-.membre {
-  display: block;
-  margin-top: 1rem;
-  margin-left: 1rem;
-  color: #ffffff;
-  font-size: 1.125rem;
-  line-height: 1.75rem;
-
-  @media (min-width: 1024px) {
-    display: inline-block;
-    margin-top: 0;
-  }
-}
-.membre:hover {
-  color: #34d399;
-}
-
-.se-deconnecter {
-  display: inline-block;
-  padding-top: 0.5rem;
-  padding-bottom: 0.5rem;
-  padding-left: 1rem;
-  padding-right: 1rem;
-  margin-top: 1rem;
-  color: #ffffff;
-  font-size: 0.875rem;
-  line-height: 1.25rem;
-  line-height: 1;
-  border-radius: 0.25rem;
-  border-width: 1px;
-  border-color: #ffffff;
-
-  @media (min-width: 1024px) {
-    margin-top: 0;
-  }
-}
-.se-deconnecter:hover {
-  color: #34d399;
-}
-</style>
