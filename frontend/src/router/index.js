4ab1cb2699d378d019d68f6fcccffc632d276b97
Yayou <yanes75@hotmail.fr>
2022-11-08 20:25:31 +0100

    added(views:Account.vue,Feed.vue,User.vue)

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Account from '../views/Account.vue'
+import Feed from '../views/Feed.vue'
+import Users from '../views/Users.vue'
+import Login from '../components/Login.vue'
+import Signup from '../components/Signup.vue'
 import Welcome from '../components/Welcome.vue'
-
+import NotFound from '../views/NotFound.vue'
 
 Vue.use(VueRouter)
 
@@ -12,16 +17,46 @@ const routes = [{
     component: Home
   },
   {
+    path: '/profile/:id',
+    name: 'Account',
+    component: Account
+  },
+  {
+    path: '/Users',
+    name: 'Users',
+    component: Users
+  },
+  {
+    path: '/feed',
+    name: 'Feed',
+    component: Feed
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
+  },
+  {
     path: '/welcome',
     name: 'Welcome',
     component: Welcome
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound
   }
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes,
+  routes
 })
 
 export default router
